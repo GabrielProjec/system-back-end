a7e252ebc633a5981e31c2085b5f8cbc990eb7af
GabrielProjec <gabriel.acesso3@gmail.com>
2023-12-01 17:26:13 -0300

    server

--- server.js
+++ server.js
@@ -8,8 +8,19 @@ const cookieParser = require("cookie-parser")
 
 const app = express()
 
+//middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: false}))
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(
+    cors({
+        origin:[""]
+    })
+)
+
 app.get('/', (req, res) => {
-    res.send("Home Page")
+    res.send("Hello Word")
 })
 
 const PORT = 5000
