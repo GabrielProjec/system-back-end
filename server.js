f902e5a46f986c820db0160c3aef1e89ec158c42
GabrielProjec <gabriel.acesso3@gmail.com>
2023-12-03 22:26:30 -0300

    feat: UserController and UserRoutes Implemented

--- server.js
+++ server.js
@@ -1,35 +1,40 @@
 require("dotenv").config();
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const bodyParser = require("body-parser")
-const cookieParser = require("cookie-parser")
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const userRoutes = require("./src/routes/UserRouter");
 
-
-const app = express()
+const app = express();
 
 //middlewares
-app.use(express.json())
-app.use(express.urlencoded({ extended: false}))
-app.use(cookieParser())
-app.use(bodyParser.json())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(bodyParser.json());
 app.use(
-    cors({
-        origin:["http://localhost:3000"],
-        credentials: true
-    })
-)
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+);
 
-app.get('/', (req, res) => {
-    res.send("Hello Word")
-})
+//routes
 
-const PORT = 5000
+app.use("/api/users", userRoutes);
 
-mongoose.connect(process.env.MONGO_URL)
-.then(() => {
+app.get("/", (req, res) => {
+  res.send("Hello Word");
+});
+
+const PORT = 5000;
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
     app.listen(PORT, () => {
-        console.log(`Server running on ${PORT}`)
-    })
-})
-.catch((err) => console.log(err))
+      console.log(`Server running on ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
